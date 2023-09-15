f1ecdd07a712485c6b93cda3b24baf3bfc3d1194
eghearn <eghearn05@gmail.com>
2023-09-14 22:47:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int itemprice = 10;
+        double salesTax = itemprice * .05;
+        System.out.println("The sales tax of your purchase price is " + salesTax);
+    }
+}
